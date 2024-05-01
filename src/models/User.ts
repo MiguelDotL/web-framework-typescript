4b95f107ab23b7d39adee161c9568d56ff940131
MiguelDotL <miguel.d.lozano@gmail.com>
2024-05-01 08:56:14 -0400

    extract event logic to Eventing class

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,10 +6,7 @@ interface UserProps {
     id?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-    events: { [key: string]: Callback[] } = {};
     baseUrl: 'http://localhost:3000/';
     constructor(private data: UserProps) {}
     get(propName: string): number | string {
@@ -20,21 +17,6 @@ export class User {
         Object.assign(this.data, update);
     }
 
-    on(eventName: string, callback: Callback): void {
-        const handlers = this.events[eventName] || [];
-        handlers.push(callback);
-        this.events[eventName] = handlers;
-    }
-    trigger(eventName: string): void {
-        const handlers = this.events[eventName];
-        if (!handlers || handlers.length === 0) {
-            return;
-        }
-
-        handlers.forEach((callback) => {
-            callback();
-        });
-    }
     fetch(): void {
         axios
             .get(`http://localhost:3000/users/${this.get('id')}`)
